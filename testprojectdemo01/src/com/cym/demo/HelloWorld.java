5598ce7c34b1e5f14e7b822603234de0a9877c28
ADVBoy <1396560429@qq.com>
2017-10-23 23:00:25 +0800

    Initial commit

---
+++ testprojectdemo01/src/com/cym/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.cym.demo;
+
+/**
+ * create by cym on 2017/10/23
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("Hello world");
+        }
+
+    }
+}
+
+
---
+++ testprojectdemo02/src/com/cym/demo/hello.java
@@ -0,0 +1,8 @@
+package com.cym.demo;
+
+/**
+ * Created by Mark on 2017/10/23/22:08.
+ * Project : testproject
+ */
+public class hello {
+}
